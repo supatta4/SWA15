161be5603ec1f441df65973ab5ff050d1ff62909
supatta4 <87298620+supatta4@users.noreply.github.com>
2021-10-22 20:31:13 +0700

    Add files via upload

---
+++ src/FacedePatternDemo.java
@@ -0,0 +1,12 @@
+
+public class FacedePatternDemo {
+		 public static void main(String[] args) {
+		      ShapeMaker shapeMaker = new ShapeMaker();
+
+		      shapeMaker.drawCircle();
+		      shapeMaker.drawRectangle();
+		      shapeMaker.drawSquare();		
+		   }
+	}
+
+
